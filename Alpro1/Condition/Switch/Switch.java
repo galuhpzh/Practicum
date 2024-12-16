a564415d93c92597075a8f4abf291f37821403b8
galuh <190450744+galuhpzh@users.noreply.github.com>
2024-12-17 06:04:26 +0700

    Fix Switch at Condition Alpro 1

---
+++ Alpro1/Condition/Switch/Switch.java
@@ -0,0 +1,41 @@
+package Alpro1.Condition.Switch;
+
+public class Switch {
+  public static void main(String[] args) {
+      int day = 3;
+      String dayName;
+
+      switch (day) {
+          case 1:
+              dayName = "Monday";
+              break;
+          case 2:
+              dayName = "Tuesday";
+              break;
+          case 3:
+              dayName = "Wednesday";
+              break;
+          case 4:
+              dayName = "Thursday";
+              break;
+          case 5:
+              dayName = "Friday";
+              break;
+          case 6:
+              dayName = "Saturday";
+              break;
+          case 7:
+              dayName = "Sunday";
+              break;
+          default:
+              dayName = "Invalid day";
+      }
+
+      System.out.println("Day " + day + " is " + dayName);
+
+      /*
+      Day 3 is Wednesday
+       */
+
+  }
+}
