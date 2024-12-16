346e81aca4bd6b5c469ae05480629cf05d7b324d
galuh <190450744+galuhpzh@users.noreply.github.com>
2024-12-17 05:33:11 +0700

    Fix Rhombus at Data Type, Variable & Operator

---
+++ Alpro1/VariableDataTyeOperator/Exercise/Rhombus.java
@@ -0,0 +1,27 @@
+package Alpro1.VariableDataTyeOperator.Exercise;
+import java.util.Scanner;
+
+public class Rhombus {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    try {
+
+      double diagonal1, diagonal2;
+
+      System.out.print("Diagonal 1 (Cm): ");
+      diagonal1 = scanner.nextDouble();
+
+      System.out.print("Diagonal 2 (Cm): ");
+      diagonal2 = scanner.nextDouble();
+
+      double areaKite = 0.5 * diagonal1 * diagonal2;
+
+      System.out.println("Area rhombus: " + areaKite + " Cm²");
+      
+    } catch (Exception e) {
+      System.out.println("Invalid input!.");
+    }
+    scanner.close();
+  }
+}
